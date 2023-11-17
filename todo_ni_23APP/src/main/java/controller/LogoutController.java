59279740d308cd3d9a0e6d093f2f4fce116b8454
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-11-17 12:51:42 +0900

    ログアウト機能

---
+++ todo_ni_23APP/src/main/java/controller/LogoutController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public LogoutController() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		session.invalidate();
+        res.sendRedirect("login");
+		
+	}
+
+}
